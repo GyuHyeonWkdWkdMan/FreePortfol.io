fbce90938f2549efc2ac236e0d963a905dcf21fd
QUENTIN-CHOI\CHOI <nero0213@naver.com>
2025-12-28 19:24:48 +0900

    schema add

--- docker/mysql/initdb.d/create_table.sql
+++ docker/mysql/initdb.d/create_table.sql
@@ -23,3 +23,53 @@ CREATE TABLE `introdution` (
     `modified_by` bigint,
     `is_active` boolean NOT NULL DEFAULT FALSE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `board` (
+    `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title` varchar(255) NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `created_by` bigint,
+    `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `modified_by` bigint,
+    `is_active` boolean NOT NULL DEFAULT FALSE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `board_post` (
+    `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `board_id` bigint NOT NULL,
+    `title` varchar(255) NOT NULL,
+    `content` varchar(255) NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `created_by` bigint,
+    `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `modified_by` bigint,
+    `is_active` boolean NOT NULL DEFAULT FALSE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci; 
+
+CREATE TABLE `file` (
+  `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `file_name` varchar(255) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `file_size` float NOT NULL,
+  `extension` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` bigint
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `board_file`(
+    `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `board_id` bigint NOT NULL,
+    `file_id` bigint NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `post_file`(
+    `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `post_id` bigint NOT NULL,
+    `file_id` bigint NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `board_post` ADD FOREIGN KEY (`board_id`) REFERENCES `board` (`id`);
+ALTER TABLE `board_file` ADD FOREIGN KEY (`board_id`) REFERENCES `board` (`id`);
+ALTER TABLE `board_file` ADD FOREIGN KEY (`file_id`) REFERENCES `file`(`id`);
+ALTER TABLE `post_file` ADD FOREIGN KEY (`post_id`) REFERENCES `board_post` (`id`);
+ALTER TABLE `post_file` ADD FOREIGN KEY (`file_id`) REFERENCES `file`(`id`);
